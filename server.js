70d336d9f203b7c7c7b1f05ccef7402412a784f3
knitman <124901709+knitman@users.noreply.github.com>
2026-02-03 12:47:12 +0200

    Update server.js

--- server.js
+++ server.js
@@ -12,74 +12,72 @@ let players = {};
 let turnOrder = [];
 let currentTurn = 0;
 let circleValues = [];
+let horseSteps = {};
 
-function shuffle(array) {
-    return array.sort(() => Math.random() - 0.5);
-}
-
-function newCircleValues(){
-    circleValues = shuffle([1,2,3,4,5,6]);
-}
-
-function checkAllReady() {
+function shuffle(a){ return a.sort(()=>Math.random()-0.5); }
+function newCircles(){ circleValues = shuffle([1,2,3,4,5,6]); }
+function allReady(){
     const list = Object.values(players);
-    const allReady = list.length > 0 && list.every(p => p.ready);
-
-    if (allReady) {
-        turnOrder = shuffle(list.map(p => p.color));
-        io.emit("reveal_order", turnOrder);
-
-        setTimeout(()=>{
-            newCircleValues();
-            io.emit("start_turns", turnOrder[currentTurn]);
-        },4000);
-    }
+    return list.length>0 && list.every(p=>p.ready);
 }
 
-io.on("connection", (socket) => {
+io.on("connection",(socket)=>{
 
-    socket.on("choose_color", (color) => {
-        const taken = Object.values(players).find(p => p.color === color);
-        if (!taken) {
-            players[socket.id] = { color, ready: false };
+    socket.on("choose_color",(color)=>{
+        const taken = Object.values(players).find(p=>p.color===color);
+        if(!taken){
+            players[socket.id]={color,ready:false};
             socket.emit("color_ok");
-            io.emit("update_players", players);
-        } else {
-            socket.emit("color_taken");
+            io.emit("update_players",players);
+        }else socket.emit("color_taken");
+    });
+
+    socket.on("player_ready",()=>{
+        if(players[socket.id]){
+            players[socket.id].ready=true;
+            io.emit("update_players",players);
+
+            if(allReady()){
+                turnOrder = shuffle(Object.values(players).map(p=>p.color));
+                turnOrder.forEach(c=>horseSteps[c]=0);
+                io.emit("reveal_order",turnOrder);
+
+                setTimeout(()=>{
+                    newCircles();
+                    io.emit("start_turn",turnOrder[currentTurn]);
+                },4000);
+            }
         }
     });
 
-    socket.on("player_ready", () => {
-        if (players[socket.id]) {
-            players[socket.id].ready = true;
-            io.emit("update_players", players);
-            checkAllReady();
-        }
-    });
-
-    socket.on("pick_circle", (index) => {
+    socket.on("pick_circle",(index)=>{
         const player = players[socket.id];
-        if (!player) return;
+        if(!player) return;
 
-        const value = circleValues[index];
+        const steps = circleValues[index];
+        socket.emit("reveal_number",steps);
 
-        socket.emit("reveal_number", value);
-        io.emit("move_horse", { color: player.color, steps: value });
-
-        currentTurn = (currentTurn + 1) % turnOrder.length;
-        newCircleValues();
-
-        setTimeout(()=>{
-            io.emit("start_turns", turnOrder[currentTurn]);
-        },1000);
+        horseSteps[player.color]+=steps*13;
+        io.emit("move_horse",{color:player.color,steps});
     });
 
-    socket.on("disconnect", () => {
-        delete players[socket.id];
-        io.emit("update_players", players);
-    });
-});
+    socket.on("animation_done",(color)=>{
+        if(horseSteps[color] >= 900){
+            io.emit("winner",color);
+            setTimeout(()=>{
+                io.emit("reset_board");
+                players={};
+                turnOrder=[];
+                currentTurn=0;
+                horseSteps={};
+            },4000);
+            return;
+        }
 
-server.listen(3000, () => {
-    console.log("Server running on port 3000");
+        currentTurn=(currentTurn+1)%turnOrder.length;
+        newCircles();
+        io.emit("start_turn",turnOrder[currentTurn]);
+    });
+
 });
+server.listen(3000,()=>console.log("Server on 3000"));
